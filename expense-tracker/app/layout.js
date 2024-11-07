86398692863f4bb784ebab94242f84a8d4c15626
FruzeAMil <mc.rodriguezp12@uniandes.edu.co>
2024-11-07 10:52:39 -0500

    arreglado

--- expense-tracker/app/layout.js
+++ expense-tracker/app/layout.js
@@ -9,7 +9,7 @@ const geistSans = localFont({
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 8100",
+  weight: "100 900",
 });
 
 export const metadata = {
